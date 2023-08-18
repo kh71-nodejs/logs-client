23009de2bf4f36ca5637cabb7bbba4e9c278d99c
Kaing Dara <dara@kh71.com>
2023-08-18 11:50:32 +0700

    update env and socket connection

---
+++ README.md
@@ -0,0 +1,12 @@
+# Log Client
+
+## .env
+
+```bash
+ServerName="ឈ្មោះ server"
+SecretToken="ObjectID ដែរបានបង្កើតចេញពី log server"
+SocketProtocol="ws|wss"
+SocketServer="localhost or domain"
+SocketPort="number or undefind for domain"
+FileLogs="file1|file2"
+```
--- src/services/socket.js
+++ src/services/socket.js
@@ -3,14 +3,14 @@ const EventEmitter = require("events");
 
 const ee = new EventEmitter();
 
-const port = process.env.SocketPort || "8080";
-const SS = process.env.SocketServer || "localhost";
-const PROTOCOL = process.env.SocketProtocol || "ws";
-const socket = socketClient(process.env?.SocketDomain || `${PROTOCOL}://${SS}:${port}`, {
+const port = process.env?.SocketPort ? ":" + process.env?.SocketPort : "";
+const SS = process.env?.SocketServer || "localhost";
+const PROTOCOL = process.env?.SocketProtocol || "ws";
+const socket = socketClient(`${PROTOCOL}://${SS}${port}`, {
   transports: ["websocket"],
   auth: {
-    token: process.env.SecretToken || "token",
-    from: process.env.ServerName || "express",
+    token: process.env?.SecretToken || "token",
+    from: process.env?.ServerName || "express",
   },
 });
 socket.on("connect_error", function (err) {
